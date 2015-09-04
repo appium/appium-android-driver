6525c72df395a9c121f15dd8a515638e20318898
moizjv <moizjv@gmail.com>
2015-09-04 00:48:26 -0700

    Adding basic e2e tests and gulp e2e support

--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,93 +1,48 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import 'mochawait';
-//import * as helpers from '../../lib/android-helpers';
-//import ADB from 'appium-adb';
-//import { withMocks } from 'appium-test-support';
 import { AndroidDriver } from '../../..';
 import sampleApps from 'sample-apps';
-import ADB from 'appium-adb';
+//import ADB from 'appium-adb';
 
 let apiDemos = sampleApps('ApiDemos-debug');
 
-/*const should = */chai.should();
+chai.should();
 chai.use(chaiAsPromised);
 
-describe('driver e2e', () => {
-
-  describe('initDevice', () => {
-    it.skip('starts up an emulator if no devices running', async () => {
-      let driver = new AndroidDriver();
-      driver.caps = {
-        app: apiDemos
-      };
-
-      await driver.initDevice();
-
-      await driver.adb.isAppInstalled('io.appium.settings').should.eventually.be.true;
-      await driver.adb.isAppInstalled('io.appium.unlock').should.eventually.be.true;
-      // TODO check to see that unlock and settings apps were installed
-    });
-
-    it.skip('automatically connects to an emulator if one is running', async () => {
-      let driver = new AndroidDriver();
-      driver.adb = await ADB.createADB();
-      driver.caps = {
-        app: apiDemos
-      };
-
-      await driver.initDevice();
-
-      await driver.adb.isAppInstalled('io.appium.settings').should.eventually.be.true;
-      await driver.adb.isAppInstalled('io.appium.unlock').should.eventually.be.true;
-    });
-
-    it.skip('automatically connects to a real device if one is connected', () => {
-
-    });
-
-    it.skip('it connects to the specified udid if one is provided', () => {
-
-    });
-
-    it.skip('errors if a udid is provided but the device does not exist', () => {
-
-    });
-
-    it.skip('errors if a udid is provided but the device is already in use', () => {
-
-    });
-
+describe('createSession', function () {
+  let driver;
+  this.timeout(20000);
+  before(() => {
+    driver = new AndroidDriver();
   });
-
-  describe('startAndroidSession', () => {
-    it.skip('starts a session', async function () {
-      this.timeout(20000);
-
-      let driver = new AndroidDriver();
-      let caps = {
-        app: apiDemos
-      };
-      driver.caps = caps;
-      driver.opts = caps;
-
-      await driver.startAndroidSession(caps);
-
-      let val = await driver.bootstrap.sendCommand('action', {action: 'source'});
-      val.length.should.be.above(500);
-
-      // TODO
-    });
-
-    it.skip('throws an error if no devices ever connected', async () => {
-
-    });
+  it('should start android session focusing on default pkg and act ', async () => {
+    let caps = {
+      app: apiDemos,
+      deviceName: 'Android',
+      platformName: 'Android'
+    };
+    await driver.createSession(caps);
+    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+    appPackage.should.equal('io.appium.android.apis');
+    appActivity.should.equal('.ApiDemos');
   });
-
-  // describe('full e2e of driver', () => {
-  //   it('gets source of apiDemos using wd.js', async function () {
-  //     let androidDriver = new
-  //   });
-  // });
-
+  it('should start android session focusing on custom pkg and act ', async () => {
+    let pkg = 'io.appium.android.apis';
+    let act = '.view.SplitTouchView';
+    let caps = {
+      app: apiDemos,
+      deviceName: 'Android',
+      platformName: 'Android',
+      appPackage: pkg,
+      appActivity: act
+    };
+    await driver.createSession(caps);
+    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+    appPackage.should.equal(pkg);
+    appActivity.should.equal(act);
+  });
+  afterEach(async () => {
+    await driver.deleteSession();
+  });
 });
