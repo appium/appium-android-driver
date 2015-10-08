6823090272c066dfc973fb26c70cb741bce5dc60
Scott Dixon <scott@howrefreshing.com.au>
2015-10-08 16:08:25 -0700

    e2e - touch specs

---
+++ test/functional/api-demos/touch/drag-e2e-specs.js
@@ -0,0 +1,50 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { AndroidDriver } from '../../../..';
+import sampleApps from 'sample-apps';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let driver;
+let defaultCaps = {
+  app: sampleApps('ApiDemos-debug'),
+  deviceName: 'Android',
+  platformName: 'Android',
+  appActivity: '.view.DragAndDropDemo'
+};
+
+describe('apidemo - drag', function(){
+  before(async () => {
+    driver = new AndroidDriver();
+    await driver.createSession(defaultCaps);
+  });
+  after(async () => {
+    await driver.deleteSession();
+  });
+  it('should drag by element', async () => {
+    let dot3 = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_dot_3', false);
+    let dot2 = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_dot_2', false);
+    let gestures = [
+      {options: {element: dot3.ELEMENT}},
+      {options: {element: dot2.ELEMENT}}
+    ];
+    await driver.doTouchDrag(gestures);
+    let results = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_result_text', false);
+    await driver.getText(results.ELEMENT).should.eventually.include('Dropped');
+  });
+  it('should drag by element with an offset', async () => {
+    // reset the app
+    await driver.reset();
+
+    let dot3 = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_dot_3', false);
+    let dot2 = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_dot_2', false);
+    let gestures = [
+      {options: {element: dot3.ELEMENT, x: 5, y: 5}},
+      {options: {element: dot2.ELEMENT, x: 5, y: 5}}
+    ];
+    await driver.doTouchDrag(gestures);
+    let results = await driver.findElOrEls('id', 'io.appium.android.apis:id/drag_result_text', false);
+    await driver.getText(results.ELEMENT).should.eventually.include('Dropped');
+  });
+});
