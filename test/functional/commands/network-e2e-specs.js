e40e10cdb7ea298cb50a67d6e68dcc58a206ca75
Isaac Murchie <isaac@saucelabs.com>
2016-02-08 14:53:39 -0800

    Add tests for network connection setting

---
+++ test/functional/commands/network-e2e-specs.js
@@ -0,0 +1,38 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { AndroidDriver } from '../../../..';
+import sampleApps from 'sample-apps';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let driver;
+let caps = {
+  app: sampleApps('ApiDemos-debug'),
+  deviceName: 'Android',
+  platformName: 'Android',
+  appActivity: '.view.TextFields'
+};
+
+describe('network connection', function () {
+  this.timeout(120000);
+  before(async () => {
+    driver = new AndroidDriver();
+    await driver.createSession(caps);
+  });
+  after(async () => {
+    await driver.deleteSession();
+  });
+  describe('setNetworkConnection', () => {
+    function test (value) {
+      it(`should be able to set to ${value}`, async () => {
+        await driver.setNetworkConnection(value);
+        await driver.getNetworkConnection().should.eventually.equal(value);
+      });
+    }
+    for (let value of [1, 2, 4, 6]) {
+      test(value);
+    }
+  });
+});
