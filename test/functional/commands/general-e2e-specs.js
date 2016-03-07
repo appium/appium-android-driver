52bb3946ef2bacae1dd34976e6aa850c3cbe3954
Rubén González Alonso <ruben.gonzalezalonso@telefonica.com>
2016-03-07 11:04:22 +0100

    Add getStrings tests to verify new getDeviceLanguage method

--- test/functional/commands/general-e2e-specs.js
+++ test/functional/commands/general-e2e-specs.js
@@ -13,61 +13,82 @@ let defaultCaps = {
   platformName: 'Android'
 };
 
-describe('startActivity', function () {
-  before(async () => {
-    driver = new AndroidDriver();
-    await driver.createSession(defaultCaps);
+describe('general', function () {
+  describe('startActivity', function () {
+    before(async () => {
+      driver = new AndroidDriver();
+      await driver.createSession(defaultCaps);
+    });
+    after(async () => {
+      await driver.deleteSession();
+    });
+
+    it('should launch a new package and activity', async () => {
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal('io.appium.android.apis');
+      appActivity.should.equal('.ApiDemos');
+
+      let startAppPackage = 'io.appium.android.apis';
+      let startAppActivity = '.view.SplitTouchView';
+
+      await driver.startActivity(startAppPackage, startAppActivity);
+
+      let {appPackage: newAppPackage, appActivity: newAppActivity} = await driver.adb.getFocusedPackageAndActivity();
+      newAppPackage.should.equal(startAppPackage);
+      newAppActivity.should.equal(startAppActivity);
+    });
+    it('should be able to launch activity with custom intent parameter category', async () => {
+      let startAppPackage = 'io.appium.android.apis';
+      let startAppActivity = 'io.appium.android.apis.app.HelloWorld';
+      let startIntentCategory = 'appium.android.intent.category.SAMPLE_CODE';
+
+      await driver.startActivity(startAppPackage, startAppActivity, undefined, undefined, startIntentCategory);
+
+      let {appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appActivity.should.include('HelloWorld');
+    });
+    it('should be able to launch activity with dontStopAppOnReset = true', async () => {
+      let startAppPackage = 'io.appium.android.apis';
+      let startAppActivity = '.os.MorseCode';
+      await driver.startActivity(startAppPackage, startAppActivity,
+                                 startAppPackage, startAppActivity,
+                                 undefined, undefined,
+                                 undefined, undefined,
+                                 true);
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal(startAppPackage);
+      appActivity.should.equal(startAppActivity);
+    });
+    it('should be able to launch activity with dontStopAppOnReset = false', async () => {
+      let startAppPackage = 'io.appium.android.apis';
+      let startAppActivity = '.os.MorseCode';
+      await driver.startActivity(startAppPackage, startAppActivity,
+                                 startAppPackage, startAppActivity,
+                                 undefined, undefined,
+                                 undefined, undefined,
+                                 false);
+      let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
+      appPackage.should.equal(startAppPackage);
+      appActivity.should.equal(startAppActivity);
+    });
   });
-  after(async () => {
-    await driver.deleteSession();
-  });
+  describe('getStrings', function () {
+    before(async () => {
+      driver = new AndroidDriver();
+      let contactCaps = Object.assign({}, defaultCaps, {app: sampleApps('ContactManager')});
+      await driver.createSession(contactCaps);
+    });
+    after(async () => {
+      await driver.deleteSession();
+    });
 
-  it('should launch a new package and activity', async () => {
-    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appPackage.should.equal('io.appium.android.apis');
-    appActivity.should.equal('.ApiDemos');
-
-    let startAppPackage = 'io.appium.android.apis';
-    let startAppActivity = '.view.SplitTouchView';
-
-    await driver.startActivity(startAppPackage, startAppActivity);
-
-    let {appPackage: newAppPackage, appActivity: newAppActivity} = await driver.adb.getFocusedPackageAndActivity();
-    newAppPackage.should.equal(startAppPackage);
-    newAppActivity.should.equal(startAppActivity);
-  });
-  it('should be able to launch activity with custom intent parameter category', async () => {
-    let startAppPackage = 'io.appium.android.apis';
-    let startAppActivity = 'io.appium.android.apis.app.HelloWorld';
-    let startIntentCategory = 'appium.android.intent.category.SAMPLE_CODE';
-
-    await driver.startActivity(startAppPackage, startAppActivity, undefined, undefined, startIntentCategory);
-
-    let {appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appActivity.should.include('HelloWorld');
-  });
-  it('should be able to launch activity with dontStopAppOnReset = true', async () => {
-    let startAppPackage = 'io.appium.android.apis';
-    let startAppActivity = '.os.MorseCode';
-    await driver.startActivity(startAppPackage, startAppActivity,
-                               startAppPackage, startAppActivity,
-                               undefined, undefined,
-                               undefined, undefined,
-                               true);
-    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appPackage.should.equal(startAppPackage);
-    appActivity.should.equal(startAppActivity);
-  });
-  it('should be able to launch activity with dontStopAppOnReset = false', async () => {
-    let startAppPackage = 'io.appium.android.apis';
-    let startAppActivity = '.os.MorseCode';
-    await driver.startActivity(startAppPackage, startAppActivity,
-                               startAppPackage, startAppActivity,
-                               undefined, undefined,
-                               undefined, undefined,
-                               false);
-    let {appPackage, appActivity} = await driver.adb.getFocusedPackageAndActivity();
-    appPackage.should.equal(startAppPackage);
-    appActivity.should.equal(startAppActivity);
+    it('should return app strings', async () => {
+      let strings = await driver.getStrings('en');
+      strings.save.should.equal('Save');
+    });
+    it('should return app strings for the device language', async () => {
+      let strings = await driver.getStrings();
+      strings.save.should.equal('Save');
+    });
   });
 });
--- test/unit/commands/general-specs.js
+++ test/unit/commands/general-specs.js
@@ -1,6 +1,8 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import AndroidDriver from '../../..';
+import helpers from '../../../lib/android-helpers';
+import { withMocks } from 'appium-test-support';
 
 let driver;
 chai.should();
@@ -24,4 +26,34 @@ describe('General', () => {
       await driver.installApp('non/existent/app.apk').should.be.rejectedWith(/Could not find/);
     });
   });
+  describe('getStrings', withMocks({helpers}, (mocks) => {
+    it('should return app strings', async () => {
+      driver = new AndroidDriver();
+      driver.bootstrap = {};
+      driver.bootstrap.sendAction = function(){ return ''; };
+      mocks.helpers.expects("pushStrings")
+          .returns({'test': 'en_value'});
+      let strings = await driver.getStrings('en');
+      strings.test.should.equal('en_value');
+      mocks.helpers.verify();
+    });
+    it('should return cached app strings for the specified language', async () => {
+      driver = new AndroidDriver();
+      driver.adb = {};
+      driver.adb.getDeviceLanguage = function(){ return 'en'; };
+      driver.apkStrings.en = {'test': 'en_value'};
+      driver.apkStrings.fr = {'test': 'fr_value'};
+      let strings = await driver.getStrings('fr');
+      strings.test.should.equal('fr_value');
+    });
+    it('should return cached app strings for the device language', async () => {
+      driver = new AndroidDriver();
+      driver.adb = {};
+      driver.adb.getDeviceLanguage = function(){ return 'en'; };
+      driver.apkStrings.en = {'test': 'en_value'};
+      driver.apkStrings.fr = {'test': 'fr_value'};
+      let strings = await driver.getStrings();
+      strings.test.should.equal('en_value');
+    });
+  }));
 });
