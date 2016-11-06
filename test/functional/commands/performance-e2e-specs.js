0a480e39983e72c2f0b817d08a61ed2a22b733b7
황희선 <domich.hwang@gmail.com>
2016-11-07 00:35:29 +0900

    add unit test cases

--- test/functional/commands/performance-e2e-specs.js
+++ test/functional/commands/performance-e2e-specs.js
@@ -17,7 +17,6 @@ let caps = {
   appActivity: '.view.TextFields'
 };
 
-
 describe('performance', () => {
   before(async () => {
     driver = new AndroidDriver();
@@ -34,50 +33,60 @@ describe('performance', () => {
 
     it('should get the performancedata', async () => {
       let capability = await driver.getPerformanceDataTypes();
-      capability.should.eventually.equal(_.keys(SUPPORTED_PERFORMANCE_DATA_TYPES));
+      capability.should.eql(_.keys(SUPPORTED_PERFORMANCE_DATA_TYPES));
     });
     
     it('should throw an Error for unsupported capability data type ', () => {
-      (driver.getPerformanceData('io.appium.android.apis', 'info', 1000)).should.eventually.be.rejected;
+      (driver.getPerformanceData('io.appium.android.apis', 'info', 1000)).should.be.rejected;
     });
     
     it('should get the amount of cpu by user and kernel process', async () => {
       let cpu = await driver.getPerformanceData('io.appium.android.apis', 'cpuinfo', 1000);
       cpu.length.should.be.above(0);
-      cpu[0].should.eventually.equal(CPU_KEYS);
+      cpu[0].should.eql(CPU_KEYS);
       if (cpu.length > 1) {
         for (let i = 1; i < cpu.length; ++i){
-          cpu[0].length.should.eventually.equal(cpu[i].length);
+          cpu[0].length.should.equal(cpu[i].length);
         }
       }
     });
     it('should get the amount of memory used by the process', async () => {
       let memory = await driver.getPerformanceData('io.appium.android.apis', 'memoryinfo', 1000);
       memory.length.should.be.above(0);
-      memory[0].sohuld.eventually.equal(MEMORY_KEYS);
+      memory[0].should.eql(MEMORY_KEYS);
       if (memory.length > 1) {
         for (let i = 1; i < memory.length; ++i){
-          memory[0].length.should.eventually.equal(memory[i].length);
+          memory[0].length.should.equal(memory[i].length);
         }
       }
     });
     it('should get the remaining battery power', () => {
       let battery = driver.getPerformanceData('io.appium.android.apis', 'batteryinfo', 1000);
       battery.length.should.be.above(0);
-      battery[0].should.eventually.equal(BATTERY_KEYS);
+      battery[0].should.eql(BATTERY_KEYS);
       if (battery.length > 1) {
         for (let i = 1; i < battery.length; ++i){
-          battery[0].length.should.eventually.equal(battery[i].length);
+          battery[0].length.should.equal(battery[i].length);
         }
       }      
     });
     it('should get the network statistics', () => {
       let network = driver.getPerformanceData('io.appium.android.apis', 'networkinfo', 1000);
-      network.should.be.above(0);
-      _.indexOf(NETWORK_KEYS, network[0]).should.be.above(-1);
+      network.length.should.be.above(0);
+      
+      let compare = false;
+
+      for ( let j= 0 ; j < NETWORK_KEYS.length ; ++ j){
+        if (_.isEqual( NETWORK_KEYS[j], network[0])){
+          compare = true;
+        }
+      }
+      
+      compare.should.equal(true);
+      
       if (network.length > 1) {
         for (let i = 1; i < network.length; ++i){
-          network[0].length.should.eventually.equal(network[i].length);
+          network[0].length.should.equal(network[i].length);
         }
       }
     });
