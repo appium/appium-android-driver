7a0229dee72c6c258e92ed1d2697f0747bb87164
Isaac Murchie <isaac@saucelabs.com>
2017-01-04 08:29:15 -0500

    Clean up tests and make sure they all work

--- test/functional/commands/attribute-e2e-specs.js
+++ test/functional/commands/attribute-e2e-specs.js
@@ -1,23 +1,19 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import AndroidDriver from '../../..';
-import sampleApps from 'sample-apps';
+import DEFAULT_CAPS from '../desired';
+
 
 chai.should();
 chai.use(chaiAsPromised);
 
-let driver;
-let animationEl;
-let defaultCaps = {
-  app: sampleApps('ApiDemos-debug'),
-  deviceName: 'Android',
-  platformName: 'Android'
-};
-
 describe('apidemo - attributes', function () {
+  let driver;
+  let animationEl;
+
   before(async () => {
     driver = new AndroidDriver();
-    await driver.createSession(defaultCaps);
+    await driver.createSession(DEFAULT_CAPS);
     let animation = await driver.findElOrEls('accessibility id', 'Animation', false);
     animationEl = animation.ELEMENT;
   });
--- test/functional/commands/performance-e2e-specs.js
+++ test/functional/commands/performance-e2e-specs.js
@@ -1,21 +1,19 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import AndroidDriver from '../../..';
-import sampleApps from 'sample-apps';
-import { SUPPORTED_PERFORMANCE_DATA_TYPES, CPU_KEYS, MEMORY_KEYS, BATTERY_KEYS, NETWORK_KEYS } from '../../../lib/commands/performance.js';//SUPPORTED_PERFORMANCE_DATA_TYPES,CPU_KEYS,
+import { SUPPORTED_PERFORMANCE_DATA_TYPES, CPU_KEYS, MEMORY_KEYS, BATTERY_KEYS, NETWORK_KEYS } from '../../../lib/commands/performance';
 import _ from 'lodash';
+import DEFAULT_CAPS from '../desired';
+
 
 chai.should();
 chai.use(chaiAsPromised);
 
 let driver;
-let caps = {
-  app: sampleApps('ApiDemos-debug'),
-  deviceName: 'Android',
-  platformName: 'Android',
+let caps = _.defaults({
   appPackage: 'io.appium.android.apis',
   appActivity: '.view.TextFields'
-};
+}, DEFAULT_CAPS);
 
 describe('performance', () => {
   before(async () => {
@@ -35,11 +33,11 @@ describe('performance', () => {
       let capability = await driver.getPerformanceDataTypes();
       capability.should.eql(_.keys(SUPPORTED_PERFORMANCE_DATA_TYPES));
     });
-    
+
     it('should throw an Error for unsupported capability data type ', () => {
       (driver.getPerformanceData('io.appium.android.apis', 'info', 1000)).should.be.rejected;
     });
-    
+
     it('should get the amount of cpu by user and kernel process', async () => {
       let cpu = await driver.getPerformanceData('io.appium.android.apis', 'cpuinfo', 1000);
       cpu.length.should.be.above(0);
@@ -60,20 +58,20 @@ describe('performance', () => {
         }
       }
     });
-    it('should get the remaining battery power', () => {
-      let battery = driver.getPerformanceData('io.appium.android.apis', 'batteryinfo', 1000);
+    it('should get the remaining battery power', async () => {
+      let battery = await driver.getPerformanceData('io.appium.android.apis', 'batteryinfo', 1000);
       battery.length.should.be.above(0);
       battery[0].should.eql(BATTERY_KEYS);
       if (battery.length > 1) {
         for (let i = 1; i < battery.length; ++i){
           battery[0].length.should.equal(battery[i].length);
         }
-      }      
+      }
     });
-    it('should get the network statistics', () => {
-      let network = driver.getPerformanceData('io.appium.android.apis', 'networkinfo', 1000);
+    it('should get the network statistics', async () => {
+      let network = await driver.getPerformanceData('io.appium.android.apis', 'networkinfo', 1000);
       network.length.should.be.above(0);
-      
+
       let compare = false;
 
       for ( let j= 0 ; j < NETWORK_KEYS.length ; ++ j){
@@ -81,9 +79,9 @@ describe('performance', () => {
           compare = true;
         }
       }
-      
+
       compare.should.equal(true);
-      
+
       if (network.length > 1) {
         for (let i = 1; i < network.length; ++i){
           network[0].length.should.equal(network[i].length);
