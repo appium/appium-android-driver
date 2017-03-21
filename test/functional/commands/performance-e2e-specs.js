3f672cb49a4fb310a803d9d68f47ccd529a58863
황희선 <domich.hwang@gmail.com>
2017-03-21 23:05:52 +0900

    remove all the performance stuff that came in before

--- test/functional/commands/performance-e2e-specs.js
+++ test/functional/commands/performance-e2e-specs.js
@@ -3,7 +3,6 @@ import chaiAsPromised from 'chai-as-promised';
 import AndroidDriver from '../../..';
 import sampleApps from 'sample-apps';
 import { sleep } from 'asyncbox';
-import { SUPPORTED_PERFORMANCE_DATA_TYPES, CPU_KEYS, MEMORY_KEYS, BATTERY_KEYS, NETWORK_KEYS } from '../../../lib/commands/performance.js';//SUPPORTED_PERFORMANCE_DATA_TYPES,CPU_KEYS,
 import _ from 'lodash';
 
 chai.should();
@@ -18,7 +17,7 @@ let caps = {
   appActivity: '.view.TextFields'
 };
 
-describe('performance', () => {
+describe('recording the screen', () => {
   before(async () => {
     driver = new AndroidDriver();
     await driver.createSession(caps);
@@ -27,70 +26,11 @@ describe('performance', () => {
     await driver.deleteSession();
   });
 
-  describe('getPerformanceData', function () {
+  describe('recording the screen', function () {
     beforeEach(async () => {
       await driver.startActivity(caps.appPackage, caps.appActivity);
     });
 
-    it('should get the performancedata', async () => {
-      let capability = await driver.getPerformanceDataTypes();
-      capability.should.eql(_.keys(SUPPORTED_PERFORMANCE_DATA_TYPES));
-    });
-    
-    it('should throw an Error for unsupported capability data type ', () => {
-      (driver.getPerformanceData('io.appium.android.apis', 'info', 1000)).should.be.rejected;
-    });
-    
-    it('should get the amount of cpu by user and kernel process', async () => {
-      let cpu = await driver.getPerformanceData('io.appium.android.apis', 'cpuinfo', 1000);
-      cpu.length.should.be.above(0);
-      cpu[0].should.eql(CPU_KEYS);
-      if (cpu.length > 1) {
-        for (let i = 1; i < cpu.length; ++i){
-          cpu[0].length.should.equal(cpu[i].length);
-        }
-      }
-    });
-    it('should get the amount of memory used by the process', async () => {
-      let memory = await driver.getPerformanceData('io.appium.android.apis', 'memoryinfo', 1000);
-      memory.length.should.be.above(0);
-      memory[0].should.eql(MEMORY_KEYS);
-      if (memory.length > 1) {
-        for (let i = 1; i < memory.length; ++i){
-          memory[0].length.should.equal(memory[i].length);
-        }
-      }
-    });
-    it('should get the remaining battery power', () => {
-      let battery = driver.getPerformanceData('io.appium.android.apis', 'batteryinfo', 1000);
-      battery.length.should.be.above(0);
-      battery[0].should.eql(BATTERY_KEYS);
-      if (battery.length > 1) {
-        for (let i = 1; i < battery.length; ++i){
-          battery[0].length.should.equal(battery[i].length);
-        }
-      }      
-    });
-    it('should get the network statistics', () => {
-      let network = driver.getPerformanceData('io.appium.android.apis', 'networkinfo', 1000);
-      network.length.should.be.above(0);
-      
-      let compare = false;
-
-      for ( let j= 0 ; j < NETWORK_KEYS.length ; ++ j){
-        if (_.isEqual( NETWORK_KEYS[j], network[0])){
-          compare = true;
-        }
-      }
-      
-      compare.should.equal(true);
-      
-      if (network.length > 1) {
-        for (let i = 1; i < network.length; ++i){
-          network[0].length.should.equal(network[i].length);
-        }
-      }
-    });
     it('should start and stop recording the screen', async () => {
       let cmd, data, length, arrayList2, availableDataIndex, fileSizeBefore, fileSizeAfter;
       cmd = ['ls', '/sdcard/test.mp4', '|', 'grep', '\'No such file\''];
