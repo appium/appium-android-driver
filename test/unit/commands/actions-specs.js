04d5a6afffc495e1a15491bfa52f9ed155daa314
dan@saucelabs.com <dan@saucelabs.com>
2017-04-24 14:58:37 -0700

    Wrote unit test for commands.pullFolder
    * Wrote a test that mocks 'driver.adb.pull' so that I could test pullFolder with local files
    * Tests that it can zip a local folder to a base64 zip
    * Writes base64 zip to a zip file, unzips it and confirms it has the same contents as the original local folder

--- test/unit/commands/actions-specs.js
+++ test/unit/commands/actions-specs.js
@@ -2,7 +2,9 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import Bootstrap from 'appium-android-bootstrap';
+import path from 'path';
 import AndroidDriver from '../../..';
+import { fs, mkdirp, tempDir, zip } from 'appium-support';
 
 let driver;
 let sandbox = sinon.sandbox.create();
@@ -46,6 +48,42 @@ describe('Actions', () => {
     it('should drag without an element', () => {
       driver.drag(0, 0, 1, 1, 1, 1);
       driver.bootstrap.sendAction.calledWith('drag').should.be.true;
+    });
+  });
+  describe('pullFolder', () => {
+    it('should pull a folder and return base64 zip', async () => {
+
+      let adbPullStub;
+      const pull = async (ignore, localPath) => {
+        await mkdirp(path.resolve(localPath));
+        await fs.writeFile(path.resolve(localPath, 'a.txt'), 'hello world', {flags: 'w'});
+        await fs.writeFile(path.resolve(localPath, 'b.txt'), 'foobar', {flags: 'w'});
+      };
+
+      // Stub in driver.adb
+      if (!driver.adb) {
+        driver.adb = {pull};
+      } else {
+        adbPullStub = sinon.stub(driver.adb, 'pull', pull);
+      }
+
+      // Get the zipped buffer contents and write it to a .zip file
+      const buffer = await driver.pullFolder('/does/not/matter');
+      const zippedDir = await tempDir.openDir();
+      await fs.writeFile(path.resolve(zippedDir, 'zipped.zip'), buffer, 'base64');
+      
+      // Extract the zip file
+      const unzippedDir = await tempDir.openDir();
+      await zip.extractAllTo(path.resolve(zippedDir, 'zipped.zip'), unzippedDir);
+      await fs.readFile(path.resolve(unzippedDir, 'a.txt'), 'utf8').should.eventually.equal('hello world');
+      await fs.readFile(path.resolve(unzippedDir, 'b.txt'), 'utf8').should.eventually.equal('foobar');
+      
+      // Restore stub
+      if (adbPullStub) {
+        adbPullStub.restore();
+      } else {
+        delete driver.adb;
+      }
     });
   });
 });
