2018086893576d354b9e2e81ea4005540bce32ad
vrunoa <vruno.alassia@gmail.com>
2017-08-17 11:23:27 -0300

    add e2e tests to verify we can toggle wifi

---
+++ test/functional/settings-e2e-specs.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import _ from 'lodash';
+import AndroidDriver from '../..';
+import DEFAULT_CAPS from './desired';
+import { sleep } from 'asyncbox';
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let defaultCaps = _.defaults({
+  androidInstallTimeout: 90000,
+  browserName: 'chrome'
+}, DEFAULT_CAPS);
+
+describe.skip('toggle wifi tests', () => {
+  let driver;
+
+  describe('functional', () => {
+    before(() => {
+      driver = new AndroidDriver();
+    });
+    afterEach(async () => {
+      await driver.deleteSession();
+    });
+    it('should toggle wifi on real devices', async () => {
+      await driver.createSession(defaultCaps);
+      let isWifiOn = await driver.isWifiOn();
+      if (isWifiOn) {
+        await driver.setWifiState(0, false);
+        await sleep(500);
+        isWifiOn = await driver.isWifiOn();
+        isWifiOn.should.be.false;
+      } else {
+        await driver.setWifiState(1, false);
+        // enabling wifi takes time
+        await sleep(2500);
+        isWifiOn = await driver.isWifiOn();
+        isWifiOn.should.be.true;
+      }
+    });
+  });
+});
