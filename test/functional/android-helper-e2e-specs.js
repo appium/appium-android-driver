b8b2b7e5a33af5758705a9cd6af9ba35164090f3
Isaac A. Murchie <isaac.appium@gmail.com>
2017-09-21 14:45:51 -0400

    Add support to force upgrade settings app (#272)

--- test/functional/android-helper-e2e-specs.js
+++ test/functional/android-helper-e2e-specs.js
@@ -5,6 +5,7 @@ import helpers from '../../lib/android-helpers';
 import ADB from 'appium-adb';
 import { app } from './desired';
 import { MOCHA_TIMEOUT } from './helpers';
+import { exec } from 'teen_process';
 
 
 let opts = {
@@ -16,12 +17,15 @@ let opts = {
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('android-helpers e2e', () => {
-  describe('installApkRemotely', () => {
+describe('android-helpers e2e', function () {
+  let adb;
+  before(async function () {
+    adb = await ADB.createADB();
+  });
+  describe('installApkRemotely', function () {
     it('installs an apk by pushing it to the device then installing it from within', async function () {
       this.timeout(MOCHA_TIMEOUT);
 
-      let adb = await ADB.createADB();
       await retryInterval(10, 500, async function () {
         if (await adb.isAppInstalled(opts.appPackage)) {
           // this sometimes times out on Travis, so retry
@@ -33,14 +37,8 @@ describe('android-helpers e2e', () => {
       await adb.isAppInstalled(opts.appPackage).should.eventually.be.true;
     });
   });
-  describe('ensureDeviceLocale', () => {
-    let adb;
-    before(async function () {
-      adb = await ADB.createADB();
-
-      if (process.env.TRAVIS) return this.skip(); //eslint-disable-line curly
-    });
-    after(async () => {
+  describe('ensureDeviceLocale @skip-ci', function () {
+    after(async function () {
       await helpers.ensureDeviceLocale(adb, 'en', 'US');
     });
     it('should set device language and country', async function () {
@@ -52,6 +50,21 @@ describe('android-helpers e2e', () => {
       } else {
         await adb.getDeviceLocale().should.eventually.equal('fr-FR');
       }
+    });
+  });
+  describe('pushSettingsApp', function () {
+    const settingsPkg = 'io.appium.settings';
+    it('should be able to upgrade from settings v1 to latest', async function () {
+      await adb.uninstallApk(settingsPkg);
+
+      // get and install old version of settings app
+      await exec('npm', ['install', `${settingsPkg}@2.0.0`]);
+      await helpers.pushSettingsApp(adb, true).should.eventually.be.rejected;
+
+      // get and install latest version of settings app
+      await exec('npm', ['uninstall', settingsPkg]);
+      await exec('npm', ['install', settingsPkg]);
+      await helpers.pushSettingsApp(adb, true);
     });
   });
 });
