204df9c005c3432ae7b4fd1c9d25107b95437111
Isaac Murchie <isaac@saucelabs.com>
2017-10-05 15:30:53 -0400

    Add initial orientation functionality

--- test/functional/commands/basic/orientation-e2e-specs.js
+++ test/functional/commands/basic/orientation-e2e-specs.js
@@ -8,34 +8,68 @@ import DEFAULT_CAPS from '../../desired';
 chai.should();
 chai.use(chaiAsPromised);
 
-describe('apidemo - orientation', function () {
+describe('apidemo - orientation -', function () {
   let driver;
-  before(async () => {
-    driver = new AndroidDriver();
-    await driver.createSession(DEFAULT_CAPS);
+
+  describe('initial -', function () {
+    beforeEach(function () {
+      driver = new AndroidDriver();
+    });
+    afterEach(async function () {
+      await driver.setOrientation('PORTRAIT');
+      await driver.deleteSession();
+    });
+    it('should have portrait orientation if requested', async function () {
+      await driver.createSession(Object.assign({}, DEFAULT_CAPS, {
+        appActivity: '.view.TextFields',
+        orientation: 'PORTRAIT',
+      }));
+      await driver.getOrientation().should.eventually.eql('PORTRAIT');
+    });
+    it('should have landscape orientation if requested', async function () {
+      await driver.createSession(Object.assign({}, DEFAULT_CAPS, {
+        appActivity: '.view.TextFields',
+        orientation: 'LANDSCAPE',
+      }));
+      await driver.getOrientation().should.eventually.eql('LANDSCAPE');
+    });
+    it('should have portrait orientation if nothing requested', async function () {
+      await driver.createSession(Object.assign({}, DEFAULT_CAPS, {
+        appActivity: '.view.TextFields',
+      }));
+      await driver.getOrientation().should.eventually.eql('PORTRAIT');
+    });
   });
-  after(async () => {
-    await driver.deleteSession();
-  });
-  it('should rotate screen to landscape', async () => {
-    await driver.setOrientation('PORTRAIT');
-    await B.delay(3000);
-    await driver.setOrientation('LANDSCAPE');
-    await B.delay(3000);
-    await driver.getOrientation().should.eventually.become('LANDSCAPE');
-  });
-  it('should rotate screen to landscape', async () => {
-    await driver.setOrientation('LANDSCAPE');
-    await B.delay(3000);
-    await driver.setOrientation('PORTRAIT');
-    await B.delay(3000);
-    await driver.getOrientation().should.eventually.become('PORTRAIT');
-  });
-  it('should not error when trying to rotate to portrait again', async () => {
-    await driver.setOrientation('PORTRAIT');
-    await B.delay(3000);
-    await driver.setOrientation('PORTRAIT');
-    await B.delay(3000);
-    await driver.getOrientation().should.eventually.become('PORTRAIT');
+  describe('setting -', function () {
+    before(async function () {
+      driver = new AndroidDriver();
+      await driver.createSession(Object.assign({}, DEFAULT_CAPS, {
+        appActivity: '.view.TextFields'
+      }));
+    });
+    after(async () => {
+      await driver.deleteSession();
+    });
+    it('should rotate screen to landscape', async function () {
+      await driver.setOrientation('PORTRAIT');
+      await B.delay(3000);
+      await driver.setOrientation('LANDSCAPE');
+      await B.delay(3000);
+      await driver.getOrientation().should.eventually.become('LANDSCAPE');
+    });
+    it('should rotate screen to landscape', async function () {
+      await driver.setOrientation('LANDSCAPE');
+      await B.delay(3000);
+      await driver.setOrientation('PORTRAIT');
+      await B.delay(3000);
+      await driver.getOrientation().should.eventually.become('PORTRAIT');
+    });
+    it('should not error when trying to rotate to portrait again', async function () {
+      await driver.setOrientation('PORTRAIT');
+      await B.delay(3000);
+      await driver.setOrientation('PORTRAIT');
+      await B.delay(3000);
+      await driver.getOrientation().should.eventually.become('PORTRAIT');
+    });
   });
 });
