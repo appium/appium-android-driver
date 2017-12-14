eb94949afe4b8246f70c5e1f9950bba381530b72
Mykola Mokhnach <nick+1@wire.com>
2017-12-14 16:41:00 +0100

    Allow execution of mobile:shell if relaxed security is enabled for the driver

---
+++ test/functional/commands/execute-e2e-specs.js
@@ -0,0 +1,38 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import AndroidDriver from '../../..';
+import _ from 'lodash';
+import DEFAULT_CAPS from '../desired';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+let driver;
+let caps = _.defaults({
+  appPackage: 'io.appium.android.apis',
+  appActivity: '.view.TextFields'
+}, DEFAULT_CAPS);
+
+describe('execute', function () {
+  before(async function () {
+    driver = new AndroidDriver();
+    await driver.createSession(caps);
+  });
+  after(async function () {
+    await driver.deleteSession();
+  });
+
+  it('should fail if one tries to execute non-mobile command in native context', async function () {
+    await driver.execute('blabla').should.eventually.be.rejected;
+  });
+
+  it('should fail if one tries to execute an unknown mobile command in native context', async function () {
+    await driver.execute('mobile: blabla').should.eventually.be.rejectedWith(/Unknown mobile command/);
+  });
+
+  it('should fail if one tries to execute an unknown mobile command in native context', async function () {
+    await driver.execute('mobile: shell', {command: 'pm', args: ['list']})
+      .should.eventually.be.rejectedWith(/must have relaxed security flag set/);
+  });
+});
