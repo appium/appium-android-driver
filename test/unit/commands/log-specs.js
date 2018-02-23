488cd02dd219d5fcab0c2de0b4f7c23eeb63bcc7
Isaac Murchie <isaac@saucelabs.com>
2018-02-23 14:14:58 -0500

    USe base driver logging methods

---
+++ test/unit/commands/log-specs.js
@@ -0,0 +1,38 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import _ from 'lodash';
+import sinon from 'sinon';
+import ADB from 'appium-adb';
+import os from 'os';
+import AndroidDriver from '../../..';
+
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('commands - logging', function () {
+  let driver;
+  before(function () {
+    driver = new AndroidDriver();
+    driver.adb = new ADB();
+  });
+  describe('getLogTypes', function () {
+    it('should get log types', async function () {
+      const types = await driver.getLogTypes();
+      // all the types should be returned
+      _.xor(['logcat', 'bugreport', 'server'], types).should.eql([]);
+    });
+  });
+  describe('getLog', function () {
+    it('should get logcat logs', async function () {
+      sinon.stub(driver.adb, 'getLogcatLogs').returns(['logs']);
+      (await driver.getLog('logcat')).should.be.deep.equal(['logs']);
+      driver.adb.getLogcatLogs.restore();
+    });
+    it('should get bugreport logs', async function () {
+      sinon.stub(driver.adb, 'bugreport').returns(`line1${os.EOL}line2`);
+      (await driver.getLog('bugreport')).should.be.deep.equal(['line1', 'line2']);
+      driver.adb.bugreport.restore();
+    });
+  });
+});
