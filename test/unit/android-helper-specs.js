a6ca8eba346e0e6b57570b8b4ebe74b591951003
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2018-05-31 22:56:44 +0900

    Add warning for apppackage, appactivity and appwaitpackages (#371)
    * add warning for app
    
    * add check in appwaitactivity
    
    * apply reviews
    
    * extract validation method
    
    * use \w
    
    * tweak arguments and warning messages
    
    * combine validation methods

--- test/unit/android-helper-specs.js
+++ test/unit/android-helper-specs.js
@@ -301,6 +301,18 @@ describe('Android Helpers', function () {
         appActivity: "act"});
       mocks.adb.verify();
     });
+    it('should print warn when all parameters are already present but the format is odd', async function () {
+      // It only prints warn message
+      mocks.adb.expects('packageAndLaunchActivityFromManifest').never();
+      await helpers.getLaunchInfo(adb, {app: "foo", appPackage: "bar ", appWaitPackage: "*", appActivity: "a_act", appWaitActivity: ". "});
+      mocks.adb.verify();
+    });
+    it('should print warn when appPackage & appActivity are already present but the format is odd', async function () {
+      // It only prints warn message
+      mocks.adb.expects('packageAndLaunchActivityFromManifest').never();
+      await helpers.getLaunchInfo(adb, {app: "foo", appPackage: "bar", appActivity: "a_act "});
+      mocks.adb.verify();
+    });
     it('should return package and launch activity from manifest', async function () {
       mocks.adb.expects('packageAndLaunchActivityFromManifest').withExactArgs('foo')
         .returns({apkPackage: 'pkg', apkActivity: 'ack'});
