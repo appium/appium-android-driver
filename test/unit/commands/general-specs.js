e4fb04e032c573af7ec772737dddae5bf7aa2871
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-06-09 16:24:24 +0900

    tweak date format

--- test/unit/commands/general-specs.js
+++ test/unit/commands/general-specs.js
@@ -51,9 +51,9 @@ describe('General', function () {
   describe('getDeviceTime', function () {
     it('should return device time', async function () {
       sandbox.stub(driver.adb, 'shell');
-      driver.adb.shell.returns(' Sat Jun 09 2018 11:17:42 GTM+0900 (JST) ');
-      await driver.getDeviceTime().should.become('Sat Jun 09 2018 11:17:42 GTM+0900 (JST)');
-      driver.adb.shell.calledWithExactly(['date', '\"+%a\ %b\ %d\ %Y\ %X\ GTM%z\ \(%Z\)\"']).should.be.true;
+      driver.adb.shell.returns(' 2018-06-09T16:21:54+0900 ');
+      await driver.getDeviceTime().should.become('2018-06-09T16:21:54+0900');
+      driver.adb.shell.calledWithExactly(['date', '+%Y-%m-%dT%T%z']).should.be.true;
     });
     it('should thorws error if shell command failed', async function () {
       sandbox.stub(driver.adb, 'shell').throws();
