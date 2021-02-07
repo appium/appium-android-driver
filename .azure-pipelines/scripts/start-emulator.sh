c621614f3ff73dbccdbdc7c8bee510555fecec8a
Mykola Mokhnach <mokhnach@gmail.com>
2021-02-07 20:33:40 +0100

    Remove comment

--- .azure-pipelines/scripts/start-emulator.sh
+++ .azure-pipelines/scripts/start-emulator.sh
@@ -16,7 +16,6 @@ echo "Starting emulator"
 
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd $ANDROID_AVD -no-snapshot > /dev/null 2>&1 &
-#$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 adb wait-for-device get-serialno
 secondsStarted=`date +%s`
