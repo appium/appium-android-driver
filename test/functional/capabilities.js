701000b8db0f9d286642e66e9ad8358d369c74de
Mykola Mokhnach <mokhnach@gmail.com>
2022-03-26 17:25:29 +0100

    Freeze default caps for good

--- test/functional/capabilities.js
+++ test/functional/capabilities.js
@@ -4,20 +4,19 @@ import _ from 'lodash';
 
 const app = require.resolve('android-apidemos');
 
-const DEFAULT_CAPS = {
+const DEFAULT_CAPS = Object.freeze({
   'appium:app': app,
   'appium:deviceName': 'Android',
   platformName: 'Android',
-};
+});
 
-const CONTACT_MANAGER_CAPS = _.defaults({
+const CONTACT_MANAGER_CAPS = Object.freeze(_.defaults({
   'appium:app': path.resolve(__dirname, '..', '..', '..', 'test', 'assets', 'ContactManager.apk'),
-}, DEFAULT_CAPS);
+}, DEFAULT_CAPS));
 
-const CHROME_CAPS = _.defaults({
+const CHROME_CAPS = Object.freeze(_.omit(_.defaults({
   browserName: 'chrome',
-}, DEFAULT_CAPS);
-delete CHROME_CAPS.app;
+}, DEFAULT_CAPS), 'appium:app'));
 
 export { app, DEFAULT_CAPS, CONTACT_MANAGER_CAPS, CHROME_CAPS };
 export default DEFAULT_CAPS;
