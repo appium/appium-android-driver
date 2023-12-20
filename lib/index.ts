3057e80eee28f217852d93557ac727b9d8840e3a
Mykola Mokhnach <mokhnach@gmail.com>
2023-12-20 18:10:44 +0100

    feat: Add common android checks

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ import {AndroidDriver} from './driver';
 export type * from './commands';
 export {ANDROID_DRIVER_CONSTRAINTS as commonCapConstraints} from './constraints';
 export * from './driver';
+export * as doctor from './doctor/checks';
 export {SETTINGS_HELPER_PKG_ID, default as androidHelpers} from './helpers/android';
 export type * from './helpers/types';
 export {
