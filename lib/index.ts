59ea0dd2e79d9a965704b43e5388436c632a7601
Mykola Mokhnach <mokhnach@gmail.com>
2024-01-18 21:22:34 +0100

    Restore type exports

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import {install} from 'source-map-support';
 install();
 
 import {AndroidDriver} from './driver';
+export type * from './commands/types';
 export {ANDROID_DRIVER_CONSTRAINTS as commonCapConstraints} from './constraints';
 export * from './driver';
 export * as doctor from './doctor/checks';
