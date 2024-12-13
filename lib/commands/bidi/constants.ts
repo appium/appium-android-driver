a614829756e8cf56a98451a32062b2f5b6c6cad8
Mykola Mokhnach <mokhnach@gmail.com>
2024-12-13 21:47:11 +0100

    chore: Tune contextUpdate event generation

--- lib/commands/bidi/constants.ts
+++ lib/commands/bidi/constants.ts
@@ -1,3 +1,5 @@
 export const BIDI_EVENT_NAME = 'bidiEvent';
-export const CONTEXT_UPDATED_EVENT = 'appium.contextUpdated';
+export const CONTEXT_UPDATED_EVENT = (domain: string) => `appium:${domain}.contextUpdated`;
+/** @deprecated Use {@link CONTEXT_UPDATED_EVENT} instead */
+export const CONTEXT_UPDATED_EVENT_OBSOLETE = 'appium.contextUpdated';
 export const LOG_ENTRY_ADDED_EVENT = 'log.entryAdded';
