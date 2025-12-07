4f1f49fec3d69d63fffab75231179a56fc070895
Mykola Mokhnach <mokhnach@gmail.com>
2025-12-07 21:57:45 +0100

    chore: Ditch usage of @appium/test-support (#1033)

--- test/unit/commands/recordscreen-specs.ts
+++ test/unit/commands/recordscreen-specs.ts
@@ -1,28 +1,35 @@
 import {AndroidDriver} from '../../../lib/driver';
-import {withMocks} from '@appium/test-support';
-import {fs, tempDir} from '@appium/support';
 import {ADB} from 'appium-adb';
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
 
 use(chaiAsPromised);
 
-const driver = new AndroidDriver();
-const adb = new ADB();
-driver.adb = adb;
 describe('recording the screen', function () {
   this.timeout(60000);
 
-  describe(
-    'basic',
-    withMocks({adb, driver, fs, tempDir}, (mocks: any) => {
-      it('should fail to recording the screen on an older emulator', async function () {
-        mocks.driver.expects('isEmulator').returns(true);
-        mocks.adb.expects('getApiLevel').returns(26);
+  let driver: AndroidDriver;
+  let adb: ADB;
+  const sandbox = sinon.createSandbox();
 
-        await expect(driver.startRecordingScreen()).to.be.rejectedWith(/Screen recording does not work on emulators/);
-      });
-    })
-  );
+  beforeEach(function () {
+    driver = new AndroidDriver();
+    adb = new ADB();
+    driver.adb = adb;
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe('basic', function () {
+    it('should fail to recording the screen on an older emulator', async function () {
+      sandbox.stub(driver, 'isEmulator').returns(true);
+      sandbox.stub(adb, 'getApiLevel').returns(26);
+
+      await expect(driver.startRecordingScreen()).to.be.rejectedWith(/Screen recording does not work on emulators/);
+    });
+  });
 });
 
